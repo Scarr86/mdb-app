65d052340189872e19c1d4e485fd8bb685b7bc30
DEN <voland2114@gmail.com>
2019-06-05 00:30:58 +0700

    fun space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
+
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+   cap:number = 10;
+  on:number = 9;
+  wait:number = 3;
+  res:number = 0  ;
+
+  type:string = "button";
+
+  space($event) : void {
+      this.res = this.cap - (this.on + this.wait); 
+      console.log( this.res);
+      this.res = (this.res >= 0) ? 0 : this.res * (-1);
+      
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
@@ -12,7 +15,8 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
