58a3653ab63e5519ab984b54d40653479771afa5
Denis <voland2114@gmail.com>
2019-06-07 15:59:52 +0700

    mas square 4x3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { DataService} from './data.service';
-import { Phone } from './phone';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [DataService]
 
 })
 export class AppComponent {
-  name:string = 'myname';
-  price:number = 44;
-  items: Phone[] = [];
-  
-  constructor(private dataService: DataService){}
-
-  addItem(name: string, price: number){
-    this.dataService.addData(name, price);
-  }
-  ngOnInit(){
-    this.items = this.dataService.getData();
-  }
-  
+  constructor(){}
 }
--- src/app/csstest/csstest.component.ts
+++ src/app/csstest/csstest.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService} from '../data.service';
+import { Phone } from '../phone';
 
 @Component({
   selector: 'app-csstest',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./csstest.component.scss']
 })
 export class CsstestComponent implements OnInit {
-
-  constructor() { }
+  rowdiv:number[] = [1,2,3];
+  coldiv:number[] = [1,2,3,4];
+  name:string = 'myname';
+  price:number = 44;
+  items: Phone[] = [];
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.items = this.dataService.getData();
+  }
+  addItem(name: string, price: number){
+    this.dataService.addData(name, price);
   }
 
 }
